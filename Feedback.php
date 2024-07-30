e66cb65827f7be16f5d911f93bee59b620ac9566
LyDaoHan <150582508+LyDaoHan@users.noreply.github.com>
2024-07-31 01:54:07 +0700

    Change OwnerNotifi.css to Feedback.php

---
+++ Feedback.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="AdminStyle.css">
+  <style>
+    h2{
+        margin-bottom: 30px;
+    }
+    p{
+        line-height: 20px;   /* within paragraph */
+        margin-bottom: 20px;
+    }
+  </style>
+</head>
+<div>
+    <h2>From Customer: User123<br>
+    Package Number: AVN00</br>
+    </h2>
+    <p>
+    Package Details:
+    </p>
+    <p>    
+    Product rating: 10   
+    </p>
+    <p>
+    Customer Problems Feedback: No problems. All are good. 
+    </p>
+    <p>
+    Customer Suggestion Feedback: None.
+    </p>
+    <div>
+        Image: <img src ="img.jpg"> 
+    </div>
+    
