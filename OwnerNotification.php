14256e58d911070818fdefd5e56740751bfb2559
LyDaoHan <150582508+LyDaoHan@users.noreply.github.com>
2024-07-31 01:51:47 +0700

    Update and rename OwnerNotification.html to OwnerNotification.php

---
+++ OwnerNotification.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="AdminStyle.css">
+  <title>Notification Page</title>
+</head>
+<body>
+<?php include 'OwnerSidebar.php'; ?>
+<div class="container">
+<div class="inner_left_pannel">
+  <div class="header-left">
+    <select name="user_type">
+      <option value="Inbox">Inbox</option>
+      <option value="Feedback">Feedback</option>
+    </select>
+    <div>Mark all as read</div>
+  </div>
+  <div class="notification-list">
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification unread">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="inner_right_pannel">
+  <h1>Detail</h1>
+  <?php include 'feedback.php'; ?>
+  
+  
+</div>
+</div>
+</body>
+</html>
