4d9ae1025672f9df2bacedebd23ad6ec1d575a08
LyDaoHan <150582508+LyDaoHan@users.noreply.github.com>
2024-08-28 20:39:37 +0700

    upload notification content

---
+++ role/owner/NoOrder.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="AdminStyle.css">
+  <style>
+    h2{
+        margin-bottom: 30px;
+    }
+    p{
+        line-height: 20px;   /* within paragraph */
+        margin-bottom: 20px;
+    }
+  </style>
+</head>
+<h2>
+    You don't have any order yet !
+</h2>
+    
---
+++ role/owner/ViewFeedback.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="AdminStyle.css">
+  <title>Notification Page</title>
+</head>
+<body>
+<?php include 'OwnerSidebar.php'; ?>
+<div class="container">
+<div class="inner_left_pannel">
+  <div class="header-left">
+    <select onchange="redirectPage()" id="select" name="user_type">
+      <option value="ViewFeedback.php">Feedback</option>
+      <option value="OwnerNotification.php">Income Order</option>
+    </select> 
+    <div>Mark all as read</div>
+  </div>
+  <div class="notification-list">
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification unread">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+    <div class="notification">
+      <img alt="photo" src="1.jpg">
+      <div class="description">
+        <div class="name">cao nguyen huy hoang</div>
+        <article>ersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawefersgsergaefawefawefawefawefawef</article>
+        <span>Date</span>      
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="inner_right_pannel">
+  <h1>Detail</h1>
+  <?php
+include '../../ConnectDB.php';
+
+if(!isset($_SESSION)){
+  session_start();
+}
+
+$sql = "SELECT * FROM feedback";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0){
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        $id = $row["id"];
+        $rate = $row["rate"];
+        $from = $_SESSION["cus_id"];
+        $from2 = $row['fullname'];
+        $image = $row["image1"];
+        $problemDetail = $row["problem_detail"];
+
+        $imagePath = '../../img/' . $image; //lay anh trong file duoc up
+
+        include 'feedback.php';
+    }
+    }   else    {
+      include 'nofeedback.php';
+    }
+$conn->close();
+
+?>
+<script>
+  function redirectPage() {
+        var selectedValue = document.getElementById("select").value;
+        window.location.href = selectedValue;
+    }
+
+  
+</script> 
+  
+</div>
+</div>
+</body>
+</html>
