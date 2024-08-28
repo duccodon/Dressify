55c087a711a350416d729eeb50f69878c039b63b
LyDaoHan <150582508+LyDaoHan@users.noreply.github.com>
2024-08-28 20:38:23 +0700

    Update IncomeOrder.php

--- role/owner/IncomeOrder.php
+++ role/owner/IncomeOrder.php
@@ -1,35 +1,3 @@
-<?php
-include '../../ConnectDB.php';
-
-if(!isset($_SESSION)){
-  session_start();
-}
-
-$sql = "SELECT * FROM orders";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0){
-    //output data of each row
-    while($row = $result->fetch_assoc()){
-        $id = $row["id"];
-        $from = $_SESSION['cus_id'];
-        $to = $_SESSION['cus_id'];
-        $date = $row["date_start"];
-        $duration = $row["duration"];
-        $status = $row["status"];
-        $shipadd = $row["shipping_address"];
-        $delivercode = $row["delivery_code"];
-
-    }
-    }   else    {
-        echo "0 results";
-    }
-$conn->close();
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,26 +16,21 @@ $conn->close();
 </head>
 <div>
     <h2>From Customer: <?php echo $from; ?><br>
-    Package Number: AVN00</br>
+    Package Number: <?php echo $id; ?> </br>
     </h2>
     <p>Dear owner</p>
-    <p>We are delighted to inform you that there is a new booking for your product. A customer named <?php echo $from; ?> has expressed interest in renting your item.</p>
+    <p>We are delighted to inform you that there is a new booking for your product. A customer named <?php echo $from2; ?> has expressed interest in renting your item.</p>
    <fieldset>
     <legend>Details</legend>
-    <p>Package Details: <?php echo $date;?>
-    <?php echo $duration; ?>.</p>
+    <p>Package Details: <br>Date Start: <?php echo $date;?>.<br>Duration: <?php echo $duration; ?> days.</p>
     <br>
-    <p>Package Note:</p>
+    <p>Package Note: <br> <?php echo $note;?></p>
     <br>    
     <p>Shipping Address: <?php echo $shipadd; ?>.</p>
-    <br>  
-    <p>Delivery Code: <?php echo $delivercode;?>.</p>
     <br>
-    <div>
-        <p>Image:</p> 
-        <img src ="inter.png"> 
-    </div>
+    <p>Contact number: <?php echo $contact_number; ?>.</p>
+    <p>Delivery Company: <?php echo $delivercode;?>.</p>
+    <br>
     </fieldset>
     <p>Please prepare your product package as soon as possible and contact admin to start the delivery process.</p>
     <p>Best Regards</p>
-    
